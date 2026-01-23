63f582e2f66a90b383eb750ef875d832ff4adfcc
wd006 <120404806+wd006@users.noreply.github.com>
2026-01-24 00:29:40 +0300

    feat: add core application initializer and config

---
+++ config/index.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    
+    'name' => 'qURL',
+    'service' => 'URL Shortener',
+    'url'=>'https://qurl.pp.ua/',
+    'domain'=>'qurl.pp.ua',
+    'slogan' => 'Secure & customizable URL Shortening',
+    'slogan2' => 'Free, Secure & Customizable URL Shortener.'
+
+];
---
+++ init.php
@@ -0,0 +1,36 @@
+<?php
+
+define('ROOTDIR', $_SERVER['DOCUMENT_ROOT']);
+define('DEBUGMODE', true);
+
+$configData = require ROOTDIR . '/config/index.php';
+define('CONFIG', $configData);
+
+if (DEBUGMODE) {
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+}
+
+if (file_exists(ROOTDIR . '/src/helpers/functions.php')) {
+    require_once ROOTDIR . '/src/helpers/functions.php';
+}
+
+// AUTOLOADER
+spl_autoload_register(function ($className) {
+    // Class Request -> Request.php
+    $file = $className . '.php';
+
+    $directories = [
+        ROOTDIR . '/src/core/',
+        ROOTDIR . '/src/helpers/',
+        ROOTDIR . '/src/services/',
+        ROOTDIR . '/src/utils/'
+    ];
+
+    foreach ($directories as $dir) {
+        if (file_exists($dir . $file)) {
+            require_once $dir . $file;
+            return;
+        }
+    }
+});
