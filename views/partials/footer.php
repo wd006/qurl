eec2401e5f31f5a4a8c4be9e72a2ad1aacf89306
wd006 <120404806+wd006@users.noreply.github.com>
2026-01-24 00:24:15 +0300

    feat(partials): create header and footer

---
+++ views/partials/footer.php
@@ -0,0 +1,16 @@
+<footer>
+    <p> &copy; <?= date('Y') ?> <a href="https://wd006.pp.ua/" target="_blank">wd006</a><br>The source code is licensed under <a href="/LICENSE" target="_blank">AGPL-v3</a>.</p><br>
+    <nav class="nav">
+        <a href="/about">About</a> -
+        <a href="/terms">Terms</a> -
+        <a href="/privacy">Privacy</a> -
+        <a href="/faq">FAQ</a> -
+        <a href="/contact">Contact</a>
+
+    </nav><br><br>
+    <a href="https://github.com/wd006/qurl" target="_blank"><img src="https://img.shields.io/badge/View%20on%20GitHub-orange?logo=github&logoColor=white&style=for-the-badge"></img></a>
+    <a href="/"><img src="https://img.shields.io/badge/🔗 qURL-URL%20Shortener-blue.svg?&logoColor=white&style=for-the-badge"></img></a>
+</footer>
+<script src="/assets/js/ssl-toggle.js"></script>
+</body>
+</html> <?php //<!--(c)--> ?>
---
+++ views/partials/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?= htmlspecialchars($page['description'] ?? 'qURL is a free, customizable URL Shortening service.') ?>">
+    <title>
+        <?= htmlspecialchars($title ?? 'qURL') ?>
+    </title>
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="stylesheet" href="/style.css?v=<?= filemtime((ROOTDIR . '/style.css')); ?>">
+</head>
+<script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        "url": "<?= CONFIG['url'] ?>",
+        "name": "<?= CONFIG['name'] ?>",
+        "description": "<?= CONFIG['slogan'] ?>"
+    }
+</script>
+
+<body>
