49d13165798fd6c4bf8fde587c52af22da37f4d8
wd006 <120404806+wd006@users.noreply.github.com>
2026-01-27 00:46:55 +0300

    feat(utils): create error and success functions
    The "error" and "success" functions were created to manage the server's
    error and success status.

--- src/utils/Response.php
+++ src/utils/Response.php
@@ -3,5 +3,32 @@
 class Response
 {
 
-    
+    public static function error(int $code = 500, string $message, string $details)
+    {
+
+        http_response_code($code);
+        header('Content-Type: application/json; charset=utf-8');
+
+        return json_encode([
+            "status" => "error",
+            "error" => [
+                "code" => $code,
+                "message" => $message,
+                "details" => $details
+            ]
+        ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    }
+
+
+    public static function success(int $code = 200, array $data = [])
+    {
+        http_response_code($code);
+        header('Content-Type: application/json; charset=utf-8');
+
+        return json_encode([
+            "status" => "success",
+            "code"   => $code,
+            "data"   => $data
+        ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    }
 }
