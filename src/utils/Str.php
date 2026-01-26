a1ded716382fae54bb2ee339fc747bbd312bea36
wd006 <120404806+wd006@users.noreply.github.com>
2026-01-27 00:36:28 +0300

    feat(utils): create random string function
    "random" function that returns a randomized string of the desired length
     was created within Str.

--- src/utils/Str.php
+++ src/utils/Str.php
@@ -3,5 +3,11 @@
 class Str
 {
 
+    public static function random(int $length): string
+    {
+
+        $bytes = ceil($length / 2);
+        return substr(bin2hex(random_bytes($bytes)), 0, $length);
+    }
 
 }
