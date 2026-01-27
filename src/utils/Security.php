78acee297cdb877e54a3a64e0bd40a197120611c
wd006 <120404806+wd006@users.noreply.github.com>
2026-01-27 13:40:21 +0300

    feat(utils): create verifier function
    The "verifier" function was created to manage the decryption process of
    passwords from a single location.

--- src/utils/Security.php
+++ src/utils/Security.php
@@ -7,4 +7,9 @@ class Security
 
         return password_hash($password, PASSWORD_DEFAULT);
     }
+
+    public static function verifier(string $password, string $hash)
+    {
+        return password_verify($password, $hash);
+    }
 }
