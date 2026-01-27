fd84f6f7ed09cf734ceeb0c833140dd3b61e4706
wd006 <120404806+wd006@users.noreply.github.com>
2026-01-27 14:01:18 +0300

    feat(services): add formatUrl function
    The formatUrl function has been added to the Shortlink.php service to
    normalize long URLs.

--- src/services/Shortlink.php
+++ src/services/Shortlink.php
@@ -2,4 +2,38 @@
 
 class Shortlink
 {
+    protected function formatUrl(string $rawUrl, bool $useHttps): string
+    {
+        $domain = preg_replace('#^https?://#i', '', trim($rawUrl));
+
+        if (strpos($domain, '.') === false) { // must contain a period
+            return false;
+        }
+
+
+        if (strlen($domain) < 3) { // min length 3
+            return false;
+        }
+
+
+        $parts = explode('.', $domain);
+
+        if (count($parts) < 2) { // example. -> invalid
+            return false;
+        }
+
+        //
+        // $tld = end($parts);  
+
+        // if (strlen($tld) < 2) { // example.a -> invalid
+        //     return false;
+        // }
+        //
+
+        if (!filter_var($domain, FILTER_VALIDATE_URL)) {
+            return false;
+        }
+
+        return $domain;
+    }
 }
