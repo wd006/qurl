617f076e31f81da97436fe18636131b0d770b3bc
wd006 <120404806+wd006@users.noreply.github.com>
2026-01-28 12:54:11 +0300

    feat(services): add load function to Domain

--- src/services/Domain.php
+++ src/services/Domain.php
@@ -2,4 +2,22 @@
 
 class Domain
 {
+    protected $data = [];
+
+    public function __construct()
+    {
+        $this->load();
+    }
+
+    protected function load()
+    {
+        $dataDir = ROOTDIR . '/data/domains.json';
+
+        if (file_exists($dataDir)) {
+            $dataContent = file_get_contents($dataDir);
+            if ($decoded = json_decode($dataContent, true)) {
+                $this->data = $decoded;
+            }
+        }
+    }
 }
