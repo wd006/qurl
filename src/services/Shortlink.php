293cac295872922697990d90f154b541fd71506e
wd006 <120404806+wd006@users.noreply.github.com>
2026-02-01 00:17:59 +0300

    feat(services): rename shortname variables
    - Variable names have been updated from 'shortname' to 'alias'.
    - Response messages have been updated.

--- src/services/Shortlink.php
+++ src/services/Shortlink.php
@@ -7,50 +7,51 @@ class Shortlink
     {
 
         $target_url = trim($input['long_url'] ?? '');
-        $shortname = trim($input['custom_shortname'] ?? '');
+        $alias = trim($input['custom_alias'] ?? '');
         $domain_id = $input['domain_id'] ?? 0;
 
 
         $settings = $input['settings'] ?? [];
-        $settings['use_https'] = $settings['use_https'] ?? false;
+        $settings['use_https'] = isset($settings['use_https']) ? true : false;
 
 
         $admin = $input['admin'] ?? [];
-        $admin['enabled'] = $admin['enabled'] ?? false;
+        $admin['enabled'] = isset($admin['enabled']) ? true : false;
         $admin['password'] = !empty($admin['password']) ? Security::hasher($admin['password']) : null;
 
 
         $protection = $input['protection'] ?? [];
-        $protection['enabled'] = $protection['enabled'] ?? false;
+        $protection['enabled'] = isset($protection['enabled']) ? true : false;
         $protection['password'] = !empty($protection['password']) ? Security::hasher($protection['password']) : null;
 
 
         $preview = $input['preview'] ?? [];
-        $preview['enabled'] = $preview['enabled'] ?? false;
+        $preview['enabled'] = isset($preview['enabled']) ? true : false;
         $preview['note'] = $preview['note'] ?? null;
 
 
 
         if (empty($target_url)) {
-            echo Response::error(400, "Missing Parameter", "Long URL can't be blank.");
+            echo Response::error(400, "Missing URL", "Please enter a link to shorten.");
             exit;
         }
 
         if (!$target_url = $this->formatUrl($target_url)) {
-            echo Response::error(400, "Invalid URL", "Long URL format is invalid.");
+            echo Response::error(400, "Invalid URL", "Please enter a valid URL.");
             exit;
         }
 
 
-        $shortname = $this->slugify($shortname);
+        $alias = $this->slugify($alias);
 
-        if (empty($shortname)) {
-            $shortname = Str::random(6);
+
+        if (empty($alias)) {
+            $alias = Str::random(6);
         }
 
 
         $link_data = [
-            "shortname" => $shortname,
+            "alias" => $alias,
             "domain_id" => $domain_id,
             "target_url" => $target_url,
             "settings" => [
@@ -74,10 +75,10 @@ class Shortlink
     }
 
 
-    public function read($domainId, $shortname)
+    public function read($domainId, $alias)
     {
-
-        $file_path = ROOTDIR . CONFIG['links_dir'] . "$domainId/$shortname.json";
+        $alias = strtolower($alias);
+        $file_path = ROOTDIR . CONFIG['links_dir'] . "$domainId/$alias.json";
         if (file_exists($file_path)) return $data = file_get_contents($file_path);
         else return false;
     }
@@ -87,34 +88,36 @@ class Shortlink
     {
 
         if (empty($link_data)) {
-            echo Response::error(500, 'Internal Server Error', 'An error occured, try again later.');
+            echo Response::error(400, 'Error', 'Data could not be fetched, please check your connection or try again later.');
             exit;
         }
 
-        $shortname = $link_data['shortname'];
+        $alias = $link_data['alias'];
         $domain_id = $link_data['domain_id'];
         $directory = ROOTDIR . CONFIG['links_dir'] . "$domain_id/";
 
         if (!is_dir($directory)) {
             if (!@mkdir($directory, 0755, true)) {
-                echo Response::error(500, "Critical Error", "Permission denied to create directory: $directory");
+                // TODO: logger -> Critical Error: Permission denied to create directory: $directory
+                echo Response::error(500, 'Failed to save link', 'Your link could not be saved, please try again later.');
                 exit;
             }
         }
 
-        $file_path = $directory . $link_data['shortname'] . '.json';
+        $file_path = $directory . strtolower($link_data['alias']) . '.json';
 
-        if ($this->read($domain_id, $shortname)) {
-            echo Response::error(409, "Shortlink Conflict", "$shortname is exist.");
+        if ($this->read($domain_id, $alias)) {
+            echo Response::error(409, 'Alias Unavailable', "Please try another one.");
             exit;
         }
 
         if (file_put_contents($file_path, json_encode($link_data, JSON_PRETTY_PRINT)) === false) {
-            echo Response::error(500, "Critical Error", "Permission denied to write file: $file_path");
+            // TODO: logger -> Critical Error -> Permission denied to write file: $file_path
+            echo Response::error(500, 'Failed to save link', 'Your link could not be saved, please try again later.');
             exit;
         }
 
-        echo Response::success(201, $link_data);
+        echo Response::success(201, 'Link created successfully!', $link_data);
         exit;
     }
 
