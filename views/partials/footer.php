7d80feab8043f78274fe68a65f3d63d3af3ab940
wd006 <120404806+wd006@users.noreply.github.com>
2026-02-01 00:59:43 +0300

    feat(partials): add SWAL CDN script to footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,17 +1,35 @@
 <footer>
-    <p> &copy; <?= date('Y') ?> <a href="https://wd006.pp.ua/" target="_blank">wd006</a><br>The source code is licensed under <a href="/LICENSE" target="_blank">AGPL-v3</a>.</p><br>
-    <nav class="nav">
-        <a href="/about">About</a> -
-        <a href="/terms">Terms</a> -
-        <a href="/privacy">Privacy</a> -
-        <a href="/faq">FAQ</a> -
-        <a href="/contact">Contact</a>
+    <div class="footer-legal">
+        <p>
+            &copy; <?= date('Y') ?> <a href="https://wd006.pp.ua/" target="_blank">wd006</a>
+            <br>
+            The source code is licensed under <a href="/LICENSE" target="_blank">AGPL-v3</a>.
+        </p>
+    </div>
 
-    </nav><br><br>
-    <a href="https://github.com/wd006/qurl" target="_blank"><img src="https://img.shields.io/badge/View%20on%20GitHub-orange?logo=github&logoColor=white&style=for-the-badge"></img></a>
-    <a href="/"><img src="https://img.shields.io/badge/🔗 qURL-URL%20Shortener-blue.svg?&logoColor=white&style=for-the-badge"></img></a>
+    <nav class="footer-nav">
+        <a href="/about">About</a>
+        <span>-</span>
+        <a href="/terms">Terms</a>
+        <span>-</span>
+        <a href="/privacy">Privacy</a>
+        <span>-</span>
+        <a href="/faq">FAQ</a>
+        <span>-</span>
+        <a href="/contact">Contact</a>
+    </nav>
+
+    <div class="footer-badges">
+        <a href="https://github.com/wd006/qurl" target="_blank"><img src="https://img.shields.io/badge/View%20on%20GitHub-orange?logo=github&logoColor=white&style=for-the-badge"></img></a>
+        <a href="/"><img src="https://img.shields.io/badge/🔗 qURL-URL%20Shortener-blue.svg?&logoColor=white&style=for-the-badge"></img></a>
+    </div>
 </footer>
+
 <script src="/assets/js/ssl-toggle.js"></script>
+<script src="/assets/js/shorten.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@4/dark.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
 </body>
-</html> <?php //<!--(c)--> ?>
+
+</html>
